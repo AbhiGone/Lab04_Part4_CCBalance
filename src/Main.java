47d44cdddf37bbd7897a5aed5894914f90ab6ad5
AbhiGone <giriabhinaya@gmail.com>
2023-09-15 09:37:22 -0400

    Part 4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        int credit = 5000;
+        double intrest = .17;
+
+        double monthOne = credit*intrest;
+        double monthOneTotal = credit + monthOne;
+        double monthTwo = (credit+monthOne)*intrest;
+        double monthTwoTotal = monthOneTotal + monthTwo;
+
+        System.out.println("your credit balance after one month is, $" + monthOneTotal + " and your intrest is, $" + monthOne + ".");
+        System.out.println("your credit balance after one month is, $" + monthTwoTotal + " and your intrest is, $" + monthTwo + ".");
     }
 }
